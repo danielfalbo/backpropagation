28aeb4991be8fcc117d512ee86b584c159cd8d6d
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-11-24 15:36:06 +0100

    introduce Value.label

--- micrograd.py
+++ micrograd.py
@@ -1,8 +1,9 @@
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _parents=(), _op='', label=''):
         self.data = data
-        self._prev = set(_children)
+        self._prev = set(_parents)
         self._op = _op
+        self.label = label
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
@@ -10,9 +11,11 @@ class Value:
     def __mul__(self, other):
         return Value(self.data * other.data, (self, other), '*')
 
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a*b + c
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = e + c; d.label = 'd'
 print(d._prev)
 print(d._op)
+print(d.label)
