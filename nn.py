2703c8013df40c0ef23c3c96bfe8355401964c96
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-11-24 18:37:48 +0100

    introduce class Layer

--- nn.py
+++ nn.py
@@ -12,7 +12,16 @@ class Neuron:
         out = act.tanh()
         return out
 
+class Layer:
+
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs
+
 if __name__ == "__main__":
+    n = Layer(2, 3)
     x = map(Value, [2.0, 3.0])
-    n = Neuron(2)
     print(n(x))
